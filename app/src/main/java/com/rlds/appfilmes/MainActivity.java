742f27d1a7fd64aa9d0e4fd10c7c7220f60dc15b
ryanl <ryanlimadev@gmail.com>
2022-08-26 13:22:18 -0300

    criando tela principal

--- app/src/main/java/com/rlds/appfilmes/MainActivity.java
+++ app/src/main/java/com/rlds/appfilmes/MainActivity.java
@@ -1,14 +1,21 @@
 package com.rlds.appfilmes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView_filmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private void  iniciarComponentes(){
+        recyclerView_filmes = findViewById(R.id.recyclerViewFilmes);
+    }
+
+
 }
